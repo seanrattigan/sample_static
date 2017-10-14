a692165a9eaf4195caae96069b828fabc510abc7
Sean Rattigan <x16106776@student.ncirl.ie>
2017-10-14 21:47:32 +0000

    Add a Static Page controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-      render html: "Hello Static World- where is my style???"
+      render html: "Hello Static World- where is my style????"
   end
   
 end
